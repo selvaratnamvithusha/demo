5f1ceb554addbd44ed000c4b3bdc36bebf1e921b
vithuselva <svithusha1998@gmail.com>
2025-10-23 16:46:50 +0530

    updatedatabase

--- src/main/java/db/TraineeDB.java
+++ src/main/java/db/TraineeDB.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 public class TraineeDB {
     private static final String URL="jdbc:mysql://127.0.0.1:3306/trainee";
-    private static final String USER="root";
+    private static final String USER="";
     private static final String PASSWORD="root";
 
     static {
